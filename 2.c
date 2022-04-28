b63239ac80d1776f4a08908a378fe17e4f77c1ea
Heeju Jang <hejang@c1r1s1.42seoul.kr>
2022-04-28 21:19:30 +0900

    a

--- 2.c
+++ 2.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   2.c                                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hejang <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 21:15:05 by hejang            #+#    #+#             */
+/*   Updated: 2022/04/28 21:19:05 by hejang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include"push_swap.h"
 
 void	sort_two_elements(void)
 {
-	printf("sa\n");
+	write(1, "sa\n", 3);
 }
 
 void	sort_three_elements(t_stack *stack_a)
@@ -10,27 +22,27 @@ void	sort_three_elements(t_stack *stack_a)
 	t_stack_node	*curr;
 
 	curr = stack_a->header_node.next;
-	if(curr->index == stack_a->current_element_count - 3)
+	if (curr->index == stack_a->current_element_count - 3)
 	{
 		curr = curr->next;
-		if(curr->index == stack_a->current_element_count - 1)
+		if (curr->index == stack_a->current_element_count - 1)
 		{
 			rra(stack_a);
 			sa(stack_a);
 		}
 	}
-	else if(curr->index == stack_a->current_element_count - 2)
+	else if (curr->index == stack_a->current_element_count - 2)
 	{
-		curr =  curr->next;
-		if(curr->index == stack_a->current_element_count - 3)
+		curr = curr->next;
+		if (curr->index == stack_a->current_element_count - 3)
 			sa(stack_a);
 		else
 			rra(stack_a);
 	}
-	else if(curr->index == stack_a->current_element_count - 1)
+	else if (curr->index == stack_a->current_element_count - 1)
 	{
 		curr = curr->next;
-		if(curr->index == stack_a->current_element_count - 3)
+		if (curr->index == stack_a->current_element_count - 3)
 			ra(stack_a);
 		else
 		{
@@ -39,4 +51,3 @@ void	sort_three_elements(t_stack *stack_a)
 		}
 	}
 }
-
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hejang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 11:00:14 by hejang            #+#    #+#             */
-/*   Updated: 2022/04/25 19:39:58 by hejang           ###   ########.fr       */
+/*   Updated: 2022/04/28 21:16:46 by hejang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@ int	main(int argc, char *argv[])
 	if (argc > 2)
 	{
 		change_arg(argc, argv, stack_a);
-		if(check_sort(stack_a) == 1)
+		if (check_sort(stack_a) == 1)
 			return (0);
-		if(sort_less_than_five(stack_a) == 1)
+		if (sort_less_than_five(stack_a) == 1)
 			return (0);
 		sort_stack(stack_a);
 	}
 	return (0);
 }
 
-int sort_less_than_five(t_stack *stack_a)
+int	sort_less_than_five(t_stack *stack_a)
 {
-	if(!stack_a)
+	if (!stack_a)
 		return (-1);
-	if(stack_a->current_element_count == 2)
+	if (stack_a->current_element_count == 2)
 		sort_two_elements();
-	else if(stack_a->current_element_count == 3)
+	else if (stack_a->current_element_count == 3)
 		sort_three_elements(stack_a);
 /*	else if(stack_a->current_element_count == 4)
 		sort_four_elements(stack_a);
